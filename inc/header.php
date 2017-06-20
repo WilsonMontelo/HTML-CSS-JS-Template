4fa4d3830a7e65c3a09ecd3239b3534dc7355065
Wilson Montelo <contato@wilsonmontelo.com>
2017-06-20 10:59:37 -0300

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt">
   <head>
-    <title>Sistema de Ensino Equipe - SEE</title>
+    <title></title>
     <meta charset="utf-8">
     <meta http-equiv="Content-Language" content="pt-br">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,49 +9,48 @@
     <meta name="description" content="?">
     <meta name="keywords" content="?">
     <meta name="author" content="?">
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- Estilos-->
     <link rel="stylesheet" href="css/estilos.css">
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- Favicon -->
     <link rel="icon" href="favicon.ico" type="image/x-icon"/>
   </head>
-
 <body>
-  <section id="topo">
+<section id="menu-principal">
+  <header>
     <div class="container" id="centraliza">
-     <nav class="navbar navbar-default">
-      <div class="container-fluid">
+      <nav class="navbar navbar-default">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">INSCRIÇÕES</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <!-- Logo -->
+            <a class="navbar-brand" href="#">Logo</a>
         </div>
-
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li><a href="#">Plugue</a></li>
-            <li><a href="#">Plugue</a></li>
-            <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Plugue <span class="caret"></span></a>
-              <ul class="dropdown-menu"> 
-                <li><a href="quem-somos.php">Quem somos</a></li>
-                <li><a href="missao-educacional.php">Missão educacional</a></li>
-                <li><a href="proposta-pedagogica.php">Proposta pedagógica</a></li>
-              </ul>
-           </li>
-          </ul>
-          </div>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
-  </div>
+            <ul class="nav navbar-nav">
+              <li><a href="#">Botão</a></li>
+              <li><a href="#">Botão</a></li>
+              <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Botão <span class="caret"></span></a>
+                <ul class="dropdown-menu"> 
+                  <li><a href="quem-somos.php">Quem somos</a></li>
+                  <li><a href="missao-educacional.php">Missão educacional</a></li>
+                  <li><a href="proposta-pedagogica.php">Proposta pedagógica</a></li>
+                </ul>
+             </li>
+            </ul>
+        </div>
+      </nav>
+    </div><!-- Fim Centraliza -->
+  </header>
 </section>
-<!-- FIM TOPO -->
+<!-- Fim Menu Principal -->
